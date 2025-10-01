f192f91bfbfc61660a35eaa2ef0ae1fab9848ae1
tomogolla <tomogolla637@gmail.com>
2025-10-01 09:01:41 +0300

    finished mvp front end ui

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,9 @@ from . import views
 urlpatterns = [
 	path("", views.homepage, name="home"),
     path('about', views.about_view, name='about'),
+    path('digital-villlage', views.digital_village, name='digital-village'),
+    path('contact', views.contact_view, name='contact'),
+    path('advocacy', views.advocacy_view, name='advocacy'),
+    path('resources', views.resources_view, name='resources'),
+    path('events', views.events_view, name='events'),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,17 @@ def homepage(request):
 
 def about_view(request):
 	return render(request, 'about.html')
+
+def digital_village(request):
+	return render(request, 'digitalvillage.html')
+
+def contact_view(request):
+	return render(request, 'contact.html')
+
+def advocacy_view(request):
+	return render(request, 'comming_soon.html')
+
+def resources_view(request):
+	return render(request, 'comming_soon.html')
+def events_view(request):
+	return render(request, 'comming_soon.html')
